b26455382122ab1db33dfb6d4316ab7a196ef632
Кирито Асумо <kirito.asumo@gmail.com>
2023-07-19 02:32:11 +0300

    Добавлена возможность создавать шаблон папки текущего дня.

--- Today/Program.cs
+++ Today/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Spark108.Today
 {
@@ -10,24 +11,75 @@ namespace Spark108.Today
         [STAThread]
         public static void Main(string[] args)
         {
+            var defFormat = @"Y\m.M\d.m";
             var dt = DateTime.Now;
 
-            if (args.Length > 0 && args[0].Length > 2 && args[0][0] == 'n' && int.TryParse(args[0].Substring(2), out int n))
-            {
-                if (args[0][1] == '+')
+            foreach (var arg in args) 
+            { 
+                if (arg.StartsWith("n"))
                 {
-                    dt = DateTime.Now.AddDays(n);
+                    if (arg.Length > 2 && int.TryParse(args[0].Substring(2), out int n))
+                    {
+                        if (arg[1] == '+')
+                        {
+                            dt = DateTime.Now.AddDays(n);
+                        }
+                        else if (arg[1] == '-')
+                        {
+                            dt = DateTime.Now.AddDays(-n);
+                        }
+                    }
                 }
-                else if (args[0][1] == '-')
+                else if (arg.StartsWith("-format="))
                 {
-                    dt = DateTime.Now.AddDays(-n);
+                    //var line = string.Join(" ", args);
                 }
             }
 
-            var dir = new DirectoryInfo(@$"{Environment.CurrentDirectory}\{dt.Year}\{dt.Month}.{getMonth(dt.Month)}\{dt.Day}");
+            var path = defFormat.Replace("Y", dt.Year.ToString());
+            path = path.Replace("M", getMonth(dt.Month));
+            path = path.Replace("m", dt.Month.ToString());
+            path = path.Replace("d", dt.Day.ToString());
+
+            var dir = new DirectoryInfo(@$"{Environment.CurrentDirectory}\{path}");
             if (!dir.Exists) 
             {
                 dir.Create();
+
+                try
+                {
+                    var templateDir = new DirectoryInfo(@$"{Environment.CurrentDirectory}\template");
+                    if (templateDir.Exists)
+                    {
+                        foreach (var templateCopyDir in GetDirectories(templateDir))
+                        {
+                            var newFullName = templateCopyDir.FullName
+                                .Replace(templateDir.FullName, dir.FullName)
+                                .Replace("%d%", dt.Day.ToString())
+                                .Replace("%m%", dt.Month.ToString())
+                                .Replace("%M%", getMonth(dt.Month))
+                                .Replace("%Y%", dt.Year.ToString());
+
+                            Directory.CreateDirectory(newFullName);
+                        }
+
+                        foreach (var templateCopyFile in GetFiles(templateDir))
+                        {
+                            var newFullName = templateCopyFile.FullName
+                                .Replace(templateDir.FullName, dir.FullName)
+                                .Replace("%d%", dt.Day.ToString())
+                                .Replace("%m%", dt.Month.ToString())
+                                .Replace("%M%", getMonth(dt.Month))
+                                .Replace("%Y%", dt.Year.ToString());
+
+                            File.Copy(templateCopyFile.FullName, newFullName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при создании дириктории из шаблона: {ex.Message}.", "Ошибка");
+                }
             }
 
             var psi = new ProcessStartInfo() 
@@ -38,6 +90,32 @@ namespace Spark108.Today
             };
 
             Process.Start(psi);
+        }
+
+        protected static DirectoryInfo[] GetDirectories(DirectoryInfo dir) 
+        {
+            List<DirectoryInfo> dirs = new();
+            dirs.AddRange(dir.GetDirectories());
+
+            foreach (DirectoryInfo dirInfo in dir.GetDirectories())
+            {
+                dirs.AddRange(GetDirectories(dirInfo));
+            }
+
+            return dirs.ToArray();
+        }
+
+        protected static FileInfo[] GetFiles(DirectoryInfo dir)
+        {
+            List<FileInfo> files = new();
+            files.AddRange(dir.GetFiles());
+
+            foreach (DirectoryInfo dirInfo in dir.GetDirectories())
+            {
+                files.AddRange(GetFiles(dirInfo));
+            }
+
+            return files.ToArray();
         }
 
         protected static string getMonth(int month)
